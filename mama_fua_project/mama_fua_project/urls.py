86217b03d27a546b97d0de84c76ced941fff7bcf
KayongoYongo <sknsammie10@gmail.com>
2024-06-10 23:15:37 +0300

    added the pages_app urls

--- mama_fua_project/mama_fua_project/urls.py
+++ mama_fua_project/mama_fua_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users_app.urls'))
+    path('user/', include('users_app.urls')),
+    path('', include('pages_app.urls')),
 ]
