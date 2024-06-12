b7d5a7e6f603af08b9d5a16de4b2dcc05b533dd9
KayongoYongo <sknsammie10@gmail.com>
2024-06-12 22:19:53 +0300

    added the urls for Create, edit and delete bookings

--- mama_fua_project/bookings_app/urls.py
+++ mama_fua_project/bookings_app/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name='home_page'),
+    path("", views.user_dashboard, name='user_dashboard'),
     path("create-booking/", views.create_booking, name='create_booking'),
+    path("edit_booking/", views.edit_booking, name='edit_booking'),
+    path("delete_booking/" , views.delete_booking, name='delete_booking')
 ]
