521b86b52cfc40f3c67d9f459d0eff7e4082738e
KayongoYongo <sknsammie10@gmail.com>
2024-06-14 20:28:14 +0300

    modified the urls for editing and deleting bookings

--- mama_fua_project/bookings_app/urls.py
+++ mama_fua_project/bookings_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.user_dashboard, name='user_dashboard'),
     path("create-booking/", views.create_booking, name='create_booking'),
-    path("edit-booking/", views.edit_booking, name='edit_booking'),
-    path("delete-booking/" , views.delete_booking, name='delete_booking'),
     path("view-my-bookings/", views.view_my_booking, name='view_my_booking'),
+    path("edit-booking/<int:id>", views.edit_booking, name='edit_booking'),
+    path("delete-booking/<int:id>" , views.delete_booking, name='delete_booking'),
 ]
