5491f29f0ce7c318b79d1c0b2a888ddf371bcd24
KayongoYongo <sknsammie10@gmail.com>
2024-06-19 14:47:49 +0300

    added extra validators for the pick up date in the booking form

--- mama_fua_project/bookings_app/forms/bookings_form.py
+++ mama_fua_project/bookings_app/forms/bookings_form.py
@@ -1,5 +1,6 @@
 from bookings_app.models import Booking
 from django import forms
+from datetime import date, timedelta
 
 class BookingsForm(forms.ModelForm):
     class Meta:
@@ -11,11 +12,23 @@ class BookingsForm(forms.ModelForm):
             'location': forms.TextInput(attrs={'placeholder': 'location', 'class': 'form-control'}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        today = date.today()
+        self.fields['pickup_date'].widget.attrs['min'] = today.isoformat()
+        self.fields['pickup_date'].widget.attrs['max'] = (today + timedelta(days=7)).isoformat()
+
     def clean_pickup_date(self):
         pickup_date = self.cleaned_data.get('pickup_date')
 
         if pickup_date is None:
             raise forms.ValidationError("The pickup date cannot remain empty")
+        
+        today = date.today()
+        if pickup_date < today:
+            raise forms.ValidationError("The pickup date cannot be in the past")
+        if pickup_date > today + timedelta(days=7):
+            raise forms.ValidationError("The pickup date cannot be more than 7 days in the future")
         
         return pickup_date
 
