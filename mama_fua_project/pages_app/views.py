3a7b6fb714ee315b33d3f91b8316db1c6c8499eb
KayongoYongo <sknsammie10@gmail.com>
2024-06-21 22:29:44 +0300

    modified the view for the landing page

--- mama_fua_project/pages_app/views.py
+++ mama_fua_project/pages_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'pages_app/index.html')
+
+def location_page(request):
+    return render(request, 'pages_app/our_location.html')
+
+def contact_page(request):
+    return render(request, 'pages_app/contact.html')
