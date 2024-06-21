2633548983105d7d074d8039732f4697e0b773f2
KayongoYongo <sknsammie10@gmail.com>
2024-06-21 22:29:10 +0300

    modified the urls for the landing page

--- mama_fua_project/pages_app/urls.py
+++ mama_fua_project/pages_app/urls.py
@@ -3,5 +3,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name='home_page')
+    path("", views.home_page, name='home_page'),
+    path("our-location/", views.location_page, name="our_location"),
+    path("contact/", views.contact_page, name='contact_page'),
 ]
